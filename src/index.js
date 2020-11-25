47479ad0a0090c35de9700a9e88b2996b090183d
Glenn Conner <mr.sharpoblunto@gmail.com>
2020-11-25 22:20:36 +0000

    Tidied up fish rendering & made the behavior more dynamic

--- src/index.js
+++ src/index.js
@@ -16,20 +16,146 @@ const matrix = new M.LedMatrix(
 }
 );
 
-let x = 64;
+
+class Drawable {
+  constructor(top, left, bottom, right) {
+    this._x = 0;
+    this._y = 0;
+    this._xo = 1.0;
+    this._yo = 1.0;
+    this._xOffset = left;
+    this._yOffset = top;
+    this._width = right - left;
+    this._height = bottom - top;
+  }
+
+  setPosition(x,y) {
+    this._x = x;
+    this._y = y;
+  }
+
+  movePosition(x,y) {
+    this._x += x*this._xo;
+    this._y += y*this._yo;
+  }
+
+  getPosition() {
+    return { x: this._x, y: this._y };
+  }
+
+  getBoundingRect() {
+    return { 
+      top: this._y + this._yOffset, 
+      left: this._x + this._xOffset, 
+      bottom: this._y + this._yOffset + this._height,
+      right: this._x + this._xOffset + this._width
+    };
+  }
+
+  setOrientation(x,y) {
+    this._xo = x;
+    this._yo = y;
+  }
+
+  getOrientation() {
+    return {x: this._xo, y: this._yo };
+  }
+
+  x(x) {
+    return this._x + x*this._xo;
+  }
+
+  y(y) {
+    return this._y + y*this._yo;
+  }
+}
+
+class Fish extends Drawable {
+  constructor() {
+    super(-7, -9, 7, 8);
+  }
+
+  draw(matrix) {
+    matrix
+      .fgColor(this._main)
+      .drawLine(this.x(8),this.y(-1),this.x(8),this.y(1))
+      .drawLine(this.x(8),this.y(1),this.x(6),this.y(1))
+      .drawLine(this.x(6),this.y(1),this.x(6),this.y(5))
+      .drawLine(this.x(6),this.y(5),this.x(-7),this.y(5))
+      .drawLine(this.x(-7),this.y(5),this.x(-7),this.y(7))
+      .drawLine(this.x(-7),this.y(7),this.x(-9),this.y(7))
+      .drawLine(this.x(-9),this.y(7),this.x(-9),this.y(-7))
+      .drawLine(this.x(-9),this.y(-7),this.x(-7),this.y(-7))
+      .drawLine(this.x(-7),this.y(-7),this.x(-7),this.y(-5))
+      .drawLine(this.x(-7),this.y(-5),this.x(6),this.y(-5))
+      .drawLine(this.x(6),this.y(-5),this.x(6),this.y(-1))
+      .drawLine(this.x(6),this.y(-1),this.x(8),this.y(-1))
+      .fill(this.x(6),this.y(-5),this.x(-7),this.y(5))
+      //fin 1
+      .drawLine(this.x(-2),this.y(5), this.x(-2), this.y(8))
+      .drawLine(this.x(-2),this.y(8), this.x(0), this.y(8))
+      .drawLine(this.x(0),this.y(8), this.x(0), this.y(5))
+      // fin 2
+      .drawLine(this.x(-2),this.y(-5), this.x(-2), this.y(-8))
+      .drawLine(this.x(-2),this.y(-8), this.x(0), this.y(-8))
+      .drawLine(this.x(0),this.y(-8), this.x(0), this.y(-5))
+      // eye
+      .fgColor(this._fin)
+      .setPixel(this.x(4), this.y(-3))
+      // fin 2 fill
+      .fill(this.x(-1),this.y(6),this.x(-1),this.y(7))
+      // fin 2 fill
+      .fill(this.x(-1),this.y(-7),this.x(-1),this.y(-6))
+      // tail fill
+      .fill(this.x(-8),this.y(-6),this.x(-8),this.y(6))
+      //nose
+      .fgColor(this._nose)
+      .setPixel(this.x(7),this.y(0));
+  }
+
+  setColors(main, fin, nose) {
+    this._main = main;
+    this._fin = fin;
+    this._nose = nose;
+  }
+}
+
+const fish = new Fish();
+fish.setPosition(
+  -8, 
+  9 + Math.round(Math.random() * 14.0)
+);
+fish.setColors(0xFFFF00, 0x0000FF, 0xFF0000);
+
 setInterval(() => {
-  x--;
-  if (x<-17) {
-    x = 64;
+  fish.movePosition(1,0);
+  const rect = fish.getBoundingRect();
+  if (rect.right < 0 || rect.left > matrix.width() -1) {
+    const orientation = fish.getOrientation();
+    fish.setOrientation(orientation.x * -1, orientation.y);
+    const position = fish.getPosition();
+    fish.setPosition(
+      position.x,
+      9 + Math.round(Math.random() * 14.0)
+    );
+    fish.setColors(
+      Math.round(Math.random() * 0xFFFFFF), 
+      Math.round(Math.random() * 0x888888), 
+      Math.round(Math.random() * 0xFFFFFF)
+    );
   }
 }, 100);
+
+
 
 matrix.afterSync((mat, dt, t) => {
   matrix
     .clear()
-    .brightness(64)
+    .brightness(32)
     .fgColor(0x222277)
     .fill();
+
+  const x = fish.getPosition().x;
 
   for (let i = 0;i < 16; ++i) {
     const offset = Math.sin(((i/16.0)+((x+17)/81.0))*2.0 * (Math.PI* 2)) * 8;
@@ -38,43 +164,8 @@ matrix.afterSync((mat, dt, t) => {
       .fill((i*4),2 + offset,(i*4),matrix.height() - 1);
   }
 
-  matrix
-    .fgColor(0xFFFF00)
-    .drawLine(x,14,x,16)
-    .drawLine(x,16,x+2,16)
-    .drawLine(x+2,16,x+2,20)
-    .drawLine(x+2,20,x+15,20)
-    .drawLine(x+15,20,x+15,22)
-    .drawLine(x+15,22,x+17,22)
-    .drawLine(x+17,22,x+17,8)
-    .drawLine(x+17,8,x+15,8)
-    .drawLine(x+15,8,x+15,10)
-    .drawLine(x+15,10,x+2,10)
-    .drawLine(x+2,10,x+2,14)
-    .drawLine(x+2,14,x,14)
-    .fill(x+2,10,x+15,20)
-    //fin 1
-    .drawLine(x+10,20, x+10, 23)
-    .drawLine(x+10,23, x+8, 23)
-    .drawLine(x+8,23, x+8, 20)
-    // fin 2
-    .drawLine(x+10,10, x+10, 7)
-    .drawLine(x+10,7, x+8, 7)
-    .drawLine(x+8,7, x+8, 10)
-    // eye
-    .fgColor(0x0000FF)
-    .setPixel(x + 4, 12)
-    // fin 2 fill
-    .fill(x+9,21,x+9,22)
-    // fin 2 fill
-    .fill(x+9,8,x+9,9)
-    // tail fill
-    .fill(x+16,9,x+16,21)
-    //mouth
-    .fgColor(0xFF0000)
-    .setPixel(x+1,15)
-  ;
-
+  matrix.brightness(64);
+  fish.draw(matrix);
 
   setTimeout(() => matrix.sync(), 0);
 });
