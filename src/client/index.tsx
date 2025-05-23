4f7ca83cd2363afa1e0ec3e63220579a4fbb4a15
Glenn <mr.sharpoblunto@gmail.com>
2025-05-23 02:57:50 -0700

    removed fullscreen vizes from client UI

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { flushSync } from "react-dom";
 import { createRoot } from "react-dom/client";
 import { IVisualization } from "../shared/viz/visualization-type.ts";
-import { FastAverageColorResult } from "fast-average-color";
 import { Visualization, useVisualizations } from "../www/visualizations.tsx";
 
 const LONGPOLL_TIMEOUT = 30000;
-const AVERAGE_COLOR_BLEND = 0.1;
 
 type RemoteState = {
   on: boolean;
@@ -110,59 +107,19 @@ function ToggleSwitch({ isOn }: { isOn: boolean }) {
   );
 }
 
-function FullscreenButton({ onClick }: { onClick: (e: any) => void }) {
-  return (
-    <button
-      className="fullscreen-button"
-      onClick={onClick}
-      aria-label="Fullscreen"
-    >
-      <div className="fullscreen-button-tl"></div>
-      <div className="fullscreen-button-tr"></div>
-      <div className="fullscreen-button-bl"></div>
-      <div className="fullscreen-button-br"></div>
-    </button>
-  );
-}
-
 function VisualizationItem({
   viz,
   onClick,
-  onToggleFullscreen,
   selected,
 }: {
   viz: IVisualization;
   onClick: () => void;
-  onToggleFullscreen: (viz: IVisualization) => void;
   selected: boolean;
 }) {
   return (
     <div className="visualization-item" onClick={onClick}>
-      <Visualization
-        viz={viz}
-        className={selected ? "selected" : ""}
-        style={
-          {
-            viewTransitionName: `${viz.name}-viz-transition`,
-          } as React.CSSProperties
-        }
-      />
+      <Visualization viz={viz} className={selected ? "selected" : ""} />
       <div className="visualization-name">{viz.name}</div>
-      <div
-        className="fullscreen-button-container"
-        style={
-          {
-            viewTransitionName: `${viz.name}-fullscreen-button-transition`,
-          } as React.CSSProperties
-        }
-      >
-        <FullscreenButton
-          onClick={(e) => {
-            e.stopPropagation();
-            onToggleFullscreen?.(viz);
-          }}
-        />
-      </div>
     </div>
   );
 }
@@ -170,103 +127,15 @@ function VisualizationItem({
 function VisualizationList() {
   const [state, setState] = useRemoteState();
   const visualizations = useVisualizations();
-  const containerRef = React.useRef<HTMLDivElement>(null);
-  const prevColorRef = React.useRef<[number, number, number, number]>([
-    0,
-    0,
-    0,
-    0,
-  ]);
-
-  const [
-    fullscreenViz,
-    setFullscreenViz,
-  ] = React.useState<IVisualization | null>(null);
-  const [previousScrollY, setPreviousScrollY] = React.useState(0);
-
   const handleToggleOn = React.useCallback(() => toggleOn(state, setState), [
     state,
     setState,
   ]);
 
-  React.useEffect(() => {
-    if (!fullscreenViz) {
-      container?.scrollTo(0, previousScrollY);
-    }
-  }, [previousScrollY, fullscreenViz]);
-
   const handleSelect = React.useCallback(
     (v: number) => selectVisualization(v, state, setState),
     [selectVisualization, state, setState]
   );
-
-  const handleToggleFullscreen = React.useCallback(
-    (viz: IVisualization | null) => {
-      const doSelect = () => {
-        setFullscreenViz(viz);
-        if (viz) {
-          setPreviousScrollY(container?.scrollTop || 0);
-        }
-      };
-      if (document.startViewTransition) {
-        document.startViewTransition(() => flushSync(() => doSelect()));
-      } else {
-        doSelect();
-      }
-    },
-    [container, setFullscreenViz, setPreviousScrollY]
-  );
-
-  const handleUpdateAverageColor = (color: FastAverageColorResult) => {
-    if (containerRef.current) {
-      if (prevColorRef.current) {
-        prevColorRef.current = [
-          prevColorRef.current[0] * (1.0 - AVERAGE_COLOR_BLEND) +
-            color.value[0] * AVERAGE_COLOR_BLEND,
-          prevColorRef.current[1] * (1.0 - AVERAGE_COLOR_BLEND) +
-            color.value[1] * AVERAGE_COLOR_BLEND,
-          prevColorRef.current[2] * (1.0 - AVERAGE_COLOR_BLEND) +
-            color.value[2] * AVERAGE_COLOR_BLEND,
-          prevColorRef.current[3] * (1.0 - AVERAGE_COLOR_BLEND) +
-            color.value[3] * AVERAGE_COLOR_BLEND,
-        ];
-        containerRef.current.style.setProperty(
-          "background-color",
-          `rgba(${prevColorRef.current[0]}, ${prevColorRef.current[1]}, ${prevColorRef.current[2]}, ${prevColorRef.current[3]})`
-        );
-      }
-    }
-  };
-
-  if (fullscreenViz) {
-    return (
-      <div ref={containerRef} className="fullscreen-container">
-        <div className="fullscreen-visualization">
-          <Visualization
-            viz={fullscreenViz}
-            calculateAverageColor={true}
-            onUpdateAverageColor={handleUpdateAverageColor}
-            audio={true}
-            style={
-              {
-                viewTransitionName: `${fullscreenViz.name}-viz-transition`,
-              } as React.CSSProperties
-            }
-          />
-          <div
-            className="fullscreen-button-container"
-            style={
-              {
-                viewTransitionName: `${fullscreenViz.name}-fullscreen-button-transition`,
-              } as React.CSSProperties
-            }
-          >
-            <FullscreenButton onClick={() => handleToggleFullscreen(null)} />
-          </div>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <>
@@ -282,7 +151,6 @@ function VisualizationList() {
           <VisualizationItem
             key={v.name}
             onClick={() => handleSelect(index)}
-            onToggleFullscreen={handleToggleFullscreen}
             selected={index === state.visualization}
             viz={v}
           />
@@ -398,94 +266,8 @@ function App() {
           border: 0px solid #ffffff;
         }
 
-        .fullscreen-visualization {
-          width: 100%;
-          height: 100%;
-          max-width: calc(100vh * 2);
-          max-height: calc(100vw / 2);
-          aspect-ratio: 2/1;
-          position: relative;
-        }
-
         .visualization-canvas.selected {
           border: 4px solid #ffffff;
-        }
-        
-        .fullscreen-button-container {
-          position: absolute;
-          bottom: 8px;
-          right: 8px;
-          transition: opacity 0.2s ease-in-out;
-          z-index: 20;
-          pointer-events: auto;
-        }
-        
-        .fullscreen-button {
-          background: rgba(0, 0, 0, 0.5);
-          border: none;
-          color: white;
-          width: 32px;
-          height: 32px;
-          padding: 8px;
-          cursor: pointer;
-          position: relative;
-        }
-
-        .fullscreen-button-tl {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 33%;
-          height: 33%;
-          border-top: 4px solid #ffffff;
-          border-left: 4px solid #ffffff;
-        }
-
-        .fullscreen-button-tr {
-          position: absolute;
-          top: 0;
-          right: 0;
-          width: 33%;
-          height: 33%;
-          border-top: 4px solid #ffffff;
-          border-right: 4px solid #ffffff;
-        }
-
-        .fullscreen-button-bl {
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          width: 33%;
-          height: 33%;
-          border-bottom: 4px solid #ffffff;
-          border-left: 4px solid #ffffff;
-        }
-
-        .fullscreen-button-br {
-          position: absolute;
-          bottom: 0;
-          right: 0;
-          width: 33%;
-          height: 33%;
-          border-bottom: 4px solid #ffffff;
-          border-right: 4px solid #ffffff;
-        }
-
-        .fullscreen-container {
-          width: 100vw;
-          height: 100vh;
-          position: fixed;
-          top: 0;
-          left: 0;
-          background-color: #121212;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          z-index: 100;
-        }
-
-        .fullscreen-button:hover {
-          background: rgba(0, 0, 0, 0.7);
         }
       `}</style>
       <VisualizationList />
